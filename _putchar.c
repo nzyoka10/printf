b38af81480c69a2d329587b90e5f7b79f7d76d51
“nzyoka10 <mnzyokaeric@gmail.com>
2023-06-28 01:28:14 -0700

    done

---
+++ _putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _puthchar;- write the character C to stdout
+ * @c  : the character to print
+ * 
+ * return: on success 1.
+ * on error, -1 is returned and errno is set
+ * 
+ */
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
